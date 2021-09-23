f15f870645173889840f946ceae07efe38378703
javierpb275 <javierpb275@gmail.com>
2021-09-23 19:15:49 +0200

    Sending Signup Requests From The Frontend

--- pages/api/auth/signup.js
+++ pages/api/auth/signup.js
@@ -1,7 +1,10 @@
 import { hashPassword } from "../../../lib/auth";
 import { connectDatabase } from "../../../lib/db";
 
-const handler = (req, res) => {
+const handler = async (req, res) => {
+  if (req.method !== "POST") {
+    return;
+  }
   const { email, password } = req.body;
   if (
     !email ||
@@ -14,12 +17,19 @@ const handler = (req, res) => {
   }
   const client = connectDatabase();
   const db = client.db();
-  const hashedPassword = hashPassword(password);
+  const existingUser = await db.collection('users').findOne({email});
+  if (existingUser) {
+      res,status(422).json({message: 'unable to register'});
+      client.close();
+    return;
+  }
+  const hashedPassword = await hashPassword(password);
   const result = await db.collection("users").insertOne({
     email,
     password: hashedPassword,
   });
   res.status(201).json({ message: "Created user!" });
+  client.close();
 };
 
 export default handler;
