344d18b465edf3685e24b1f20915f2e5c65d8fd4
javierpb275 <javierpb275@gmail.com>
2021-09-25 12:49:40 +0200

    sending a signin request from the frontend

--- components/auth/auth-form.js
+++ components/auth/auth-form.js
@@ -1,5 +1,6 @@
 import { useState, useRef } from "react";
 import classes from "./auth-form.module.css";
+import { signIn } from "next-auth/client";
 
 const createUser = async (email, password) => {
   const response = await fetch("/api/auth/signup", {
@@ -33,6 +34,12 @@ const AuthForm = () => {
     const enteredEmail = emailInputRef.current.value;
     const enteredPassword = passwordInputRef.current.value;
     if (isLogin) {
+      const result = await signIn("credentials", {
+        redirect: false,
+        email: enteredEmail,
+        password: enteredPassword,
+      });
+      console.log(result);
     } else {
       try {
         const result = await createUser(enteredEmail, enteredPassword);
