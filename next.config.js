a326f1a1f0a78f04589c73298184500bbdd41298
javierpb275 <javierpb275@gmail.com>
2021-09-26 16:55:22 +0200

    added env variables to next.config.js

--- next.config.js
+++ next.config.js
@@ -1,3 +1,24 @@
-module.exports = {
-  reactStrictMode: true,
-}
+const { PHASE_DEVELOPMENT_SERVER } = require("next/constants");
+
+module.exports = (phase) => {
+  if (phase === PHASE_DEVELOPMENT_SERVER) {
+    return {
+      reactStrictMode: true,
+      env: {
+        db_user: `${process.env.DB_USER}`,
+        db_user_password: `${process.env.DB_USER_PASSWORD}`,
+        db_name: `${process.env.DB_NAME}`,
+        db_cluster_name: `${process.env.DB_CLUSTER_NAME}`,
+      },
+    };
+  }
+  return {
+    reactStrictMode: true,
+    env: {
+      db_user: `${process.env.DB_USER}`,
+      db_user_password: `${process.env.DB_USER_PASSWORD}`,
+      db_name: `${process.env.DB_NAME}`,
+      db_cluster_name: `${process.env.DB_CLUSTER_NAME}`,
+    },
+  };
+};
